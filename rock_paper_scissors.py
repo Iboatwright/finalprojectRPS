c393da6e328003eae9a0851b56ffd95574870597
Ivan Boatwright <iboatwright@users.noreply.github.com>
2016-04-29 01:08:49 -0400

    Initial commit

---
+++ rock_paper_scissors.py
@@ -0,0 +1,14 @@
+# rock_paper_scissors.py
+# Exercises selected: Final Project - Rock, Paper, Scissors Game
+# Name of program: Rock, Papers, Scissors
+# Description of program: Classic game of Rock, Paper, Scissors.  The user
+#   will have the option to play against the computer or against another
+#   user.
+#
+# Ivan Boatwright
+# April 29, 2016
+
+def main():
+    pass
+    return None
+
